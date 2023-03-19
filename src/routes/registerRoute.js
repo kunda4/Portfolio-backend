f6a25406c3a17170888c629be284799a7205e9a6
Your Name <you@example.com>
2023-03-19 23:09:33 +0200

    my portfolio backend deployment on railway

--- src/routes/registerRoute.js
+++ src/routes/registerRoute.js
@@ -7,9 +7,9 @@ const router = express.Router()
 // defined route
 router.post('/register', registerController)
 router.get('/users', getUser)
-router.get('/users-get/:id', getoneUser)
-router.put('/users-update/:id', updateUser)
-router.delete('/users-delete/:id', deleteUser)
+router.get('/users/get/:id', getoneUser)
+router.put('/users/update/:id', updateUser)
+router.delete('/users/delete/:id', deleteUser)
 
 
 
